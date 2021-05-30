9f9ce73cddf1e502d7db71a04b16840c21e3a47a
Silvana-Ruiz <67821436+Silvana-Ruiz@users.noreply.github.com>
2021-05-29 20:24:54 -0700

    Jugador

---
+++ Jugador.h
@@ -0,0 +1,25 @@
+/* Silvana Dorita Ruiz Olivarría A01252346
+Luis Rodolfo Bojórquez Pineda A01250513
+TC1030
+Programación Orientada a Objetos
+Grupo 700
+29 de mayo de 2021*/
+#include "Personajes.h"
+
+class Jugador : public Personaje {
+    public:
+        Jugador();
+        Jugador(string);
+};
+
+Jugador::Jugador(){
+    nombre = "nadie";
+    monedas = 0;
+    HP = 50;
+}
+
+Jugador::Jugador(string nom): Personaje(nom) {
+    nombre = nom;
+    monedas = 0;
+    HP = 50;
+}
