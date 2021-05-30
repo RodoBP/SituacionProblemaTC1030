738f873b3a67e3d41d9afb73abbf44886747170c
Silvana-Ruiz <67821436+Silvana-Ruiz@users.noreply.github.com>
2021-05-29 20:26:13 -0700

    Monedas

---
+++ Monedas.h
@@ -0,0 +1,26 @@
+/* Silvana Dorita Ruiz Olivarría A01252346
+Luis Rodolfo Bojórquez Pineda A01250513
+TC1030
+Programación Orientada a Objetos
+Grupo 700
+29 de mayo de 2021*/
+#include "Items.h"
+
+class Moneda: public Item{
+    public:
+        Moneda();
+        void use();
+
+};
+
+Moneda::Moneda(){
+    name = "Moneda";
+    descripcion = "Has reunido riquezas!";
+    HP = 0;
+    cout<< "Conseguiste una moneda"<<endl;
+}
+
+void Moneda::use(){
+
+    cout<<"Este medallon hace algo pero el profe no nos ha dicho."<<endl;
+}
