4d84c0458e2c1a19b1181a850242a291028a6e75
Silvana-Ruiz <67821436+Silvana-Ruiz@users.noreply.github.com>
2021-05-29 20:25:26 -0700

    Troll

---
+++ Troll.h
@@ -0,0 +1,39 @@
+/* Silvana Dorita Ruiz Olivarría A01252346
+Luis Rodolfo Bojórquez Pineda A01250513
+TC1030
+Programación Orientada a Objetos
+Grupo 700
+29 de mayo de 2021*/
+#include "Personajes.h"
+#include <stdlib.h>
+#include <stdio.h> 
+#include <time.h> 
+#include <Windows.h>
+
+class Troll : public Personaje {
+    public:
+        Troll();
+        int randomMonedas();
+};
+
+Troll::Troll(){
+    nombre = "TrollMalo";
+    monedas = 0;
+    HP = 100;
+}
+
+
+int Troll::randomMonedas(){
+    srand(time(0));
+
+
+    Sleep(500);
+    srand (time(NULL));
+    int min = 20;
+    int max = 100;
+    int range = max - min + 1;
+    int monran = rand() % range + min;
+
+    monedas = monran;
+    return monedas;
+}
