39192755fe85a2ed2fb5a9405464f8e94eebc6b6
Silvana Ruiz <silvanadorita@hotmail.com>
2021-06-04 15:37:29 -0700

    Cuartos

---
+++ Cocina.h
@@ -0,0 +1,20 @@
+/* Silvana Dorita Ruiz Olivarría A01252346
+Luis Rodolfo Bojórquez Pineda A01250513
+TC1030
+Programación Orientada a Objetos
+Grupo 700
+29 de mayo de 2021*/
+
+#include "Cuartos.h"
+
+class Cocina: public Cuarto{
+    public:
+        Cocina();
+};
+
+Cocina::Cocina(){
+    descripCuarto = "Aquí va la descipción del cuarto";
+    status = false; // singnifica que el cuarto no esta completado
+    acertijo  = "Aquí va el acertijo";
+    respCorrecta  = 1; //La respuesta 1 es blablabla
+}
