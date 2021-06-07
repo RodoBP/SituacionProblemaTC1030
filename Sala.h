e581bc926562476c414d9c87a0b1f98697d27ec9
Silvana Ruiz <silvanadorita@hotmail.com>
2021-06-07 16:00:17 -0700

    ortografía

--- Sala.h
+++ Sala.h
@@ -13,7 +13,7 @@ class Sala: public Cuarto{
 };
 
 Sala::Sala(){
-    descripCuarto = "This room has a huge sofa and Tv. The Tv is turned on and tuned in a cooking channel for mystical cratures. Everything looks pretty neat.";
+    descripCuarto = "This room has a huge sofa and Tv. The Tv is turned on and tuned in a cooking channel for mythical cratures. Everything looks pretty neat.";
     status = false; // singnifica que el cuarto no esta completado
     acertijo  = "Acertijo";
     respCorrecta  = 1; //La respuesta es 1
